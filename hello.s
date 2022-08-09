3bce02963d5e7927595dfbf839186c1f6641c5ef
Gharbaoui <mohamed9gharbaoui@gmail.com>
2022-08-09 17:43:31 +0100

    loop to print string

---
+++ hello.s
@@ -0,0 +1,22 @@
+[org 0x7c00]
+
+mov ah, 0x0e
+mov bx, msg
+
+print:
+	mov al, [bx]
+	cmp al, 0
+	je .exit
+	int 0x10
+	add bx, 1
+	jmp print
+
+.exit:
+	jmp .exit
+
+msg:
+	db "intro to operating systems", 0
+
+
+times 510 - ($ - $$) db 0
+dw 0xaa55
